4a1c05210cd9dacd69e35210e2f21219f67e1a96
bucephalus41 <bucephalus41@gmail.com>
2020-08-23 11:11:31 +0900

    버튼이 눌리면 => 로그로 남긴다.

--- app/src/main/java/com/zzazzu/kotlinbasic_200823/MainActivity.kt
+++ app/src/main/java/com/zzazzu/kotlinbasic_200823/MainActivity.kt
@@ -2,10 +2,18 @@ package com.zzazzu.kotlinbasic_200823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickMeBtn.setOnClickListener {
+            it: View!
+//              클릭미 버튼이 눌리면 실행될 코드 {    }
+
+            Log.d("메인화면", "클릭 미 버튼 눌") 림
+        }
     }
 }
